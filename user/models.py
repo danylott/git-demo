4ee3df6430e7294d28ea921cee4c0e4f4cc8453d
danylott <d.tuytuyshkin@gmail.com>
2023-06-23 15:49:12 +0300

    Add user age integer field

--- user/models.py
+++ user/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
 
     def __str__(self) -> str:
         return self.last_name + " " + self.name
